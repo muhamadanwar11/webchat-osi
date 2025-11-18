8a0efce6325564dcaf45813007830191e5690319
muhamadanwar11 <manwarsanusi601@gmail.com>
2025-11-18 22:40:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ wss.on("connection", ws => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
